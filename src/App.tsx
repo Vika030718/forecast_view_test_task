9a3311789238862cb8996e178367f3d8361b1ca9
Viktoria Matveeva <vika030718@gmail.com>
2022-04-07 11:51:30 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import List from "./components/List";
+
+interface ForecastList {
+  elevation: number;
+  generationtime_ms: number;
+  hourly: {
+    temperature_2m: Array<number>;
+    time: Array<number>;
+    weathercode: Array<number>;
+  };
+  hourly_units: {
+    temperature_2m: string;
+    time: string;
+    weathercode: string;
+  };
+  latitude: number;
+  longitude: number;
+  utc_offset_seconds: number;
+}
 
 function App() {
+  const [forecast, setTemperature] = useState<ForecastList>();
+
+  useEffect(() => {
+    getForecast();
+  }, []);
+
+  const getForecast = async () => {
+    const listResp = await fetch(
+      "https://api.open-meteo.com/v1/forecast?latitude=52.2297&longitude=21.0122&hourly=temperature_2m,weathercode&timeformat=unixtime&timezone=Europe%2FBerlin"
+    );
+    const forecast: ForecastList = await listResp.json();
+    setTemperature(forecast);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto pt-6 pb-6">
+      <h1 className="text-3xl font-bold mb-3">Forecast</h1>
+      <div className="flex items-start">
+        <div className="flex-none">
+          {forecast ? (
+            <List hourly={forecast.hourly} />
+          ) : (
+            <p>Loading results...</p>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/List.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+interface IProps {
+  hourly: {
+    temperature_2m: Array<number>;
+    time: Array<number>;
+    weathercode: Array<number>;
+  };
+}
+
+const List: React.FC<IProps> = ({ hourly }) => {
+  const dysplayTable = () => {
+    let forecast: number[][] = [];
+    let hourlyArray = Object.values(hourly);
+
+    hourlyArray.forEach((item, key) => {
+      for (let i = 0; i < item.length; i++) {
+        if (!forecast[i]) {
+          forecast[i] = [];
+        }
+        forecast[i][key] = item[i];
+      }
+      return forecast;
+    });
+
+    return (
+      <table className="table-auto mx-auto text-center">
+        <thead className="text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            {Object.keys(hourly).map((item, key) => (
+              <th key={key} className="p-4">
+                {item}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {forecast.map((item, key) => {
+            return (
+              <tr
+                key={key}
+                className="dark:bg-gray-800 odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700"
+              >
+                {item.map((item, key) => (
+                  <td key={key}>{item}</td>
+                ))}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    );
+  };
+
+  return (
+    <div>
+      {hourly ? dysplayTable() : <p>Loading data...</p>}
+
+      {/* <ul>{renderList()}</ul> */}
+      {/* {hourly ? console.log(hourly.time) : null} */}
+    </div>
+  );
+};
+
+export default List;
